fcb57b6052385c533e0c0092cddbfbaec0678ba2
tiensfaziah <2300016070@webmail.uad.ac.id>
2026-05-05 12:49:49 +0700

    revisi hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,28 +8,28 @@ function Hero() {
   return (
     <section
       id="home"
-      className="relative scroll-mt-24 w-full pt-24 md:pt-28 pb-20 md:pb-32 overflow-hidden"
+      className="relative scroll-mt-24 w-full pt-20 md:pt-28 pb-16 md:pb-32 overflow-hidden"
     >
 
       {/* BACKGROUND IMAGE */}
-      <div className="absolute inset-0">
+      <div className="absolute inset-0 overflow-hidden">
         <img
-  src={heroImg}
-  alt="guru gen z mengajar digital"
-  className="w-full h-full object-cover scale-110 translate-x-20"
-/>
+          src={heroImg}
+          alt="guru gen z mengajar digital"
+          className="w-full h-full object-cover object-[75%_80%]"
+        />
       </div>
 
-      {/* GRADIENT FADE (BIAR TEKS KELIATAN) */}
+      {/* GRADIENT */}
       <div className="absolute inset-0 bg-gradient-to-r from-white via-white/80 to-transparent"></div>
 
       {/* CONTENT */}
-      <div className="relative max-w-7xl mx-auto px-6 md:px-16 py-16">
+      <div className="relative max-w-7xl mx-auto px-4 md:px-16 py-12 md:py-16">
 
         <ScrollAnimation>
           <div className="max-w-xl text-center md:text-left">
 
-            <h1 className="text-3xl md:text-5xl font-bold mb-4 md:mb-6 text-gray-800 leading-tight">
+            <h1 className="text-2xl md:text-5xl font-bold mb-4 md:mb-6 text-gray-800 leading-tight">
               Mengajar Bukan Sekadar Rutinitas,
               <br /> Jadi Guru Gen Z yang Relevan di Era Digital
             </h1>
@@ -44,12 +44,12 @@ function Hero() {
 
               <button
                 onClick={() => navigate("/login")}
-                className="bg-[#DC1416] text-white px-6 py-3 rounded-xl shadow-md hover:scale-105 transition"
+                className="bg-[#DC1416] text-white px-5 py-3 rounded-xl shadow-md hover:scale-105 transition"
               >
                 Mulai Sekarang 🚀
               </button>
 
-              <button className="border border-gray-300 bg-white/80 backdrop-blur px-6 py-3 rounded-xl hover:scale-105 transition">
+              <button className="border border-gray-300 bg-white/80 backdrop-blur px-5 py-3 rounded-xl hover:scale-105 transition">
                 Lihat Demo
               </button>
 
